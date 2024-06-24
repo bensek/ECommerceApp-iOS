477a61ab57db0dec737728db3830cbe05e0e3aa5
Ben Sekyondwa <ben.sekyondwa@external.telekom.de>
2024-06-24 15:22:51 +0200

    welcome and login screens

--- OnlineGroceries/OnlineGroceriesApp.swift
+++ OnlineGroceries/OnlineGroceriesApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct OnlineGroceriesApp: App {
     var body: some Scene {
         WindowGroup {
-            WelcomeView()
+            NavigationView {
+                WelcomeView()
+            }
         }
     }
 }
