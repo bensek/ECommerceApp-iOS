1951ee839d4253cf412fd63080ed5762afeb2f03
Ben Sekyondwa <ben.sekyondwa@external.telekom.de>
2024-06-24 11:04:17 +0200

    Initial Commit

---
+++ OnlineGroceries/OnlineGroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesApp.swift
+//  OnlineGroceries
+//
+//  Created by Ben Sekyondwa on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
