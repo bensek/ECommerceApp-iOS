2840e51302a840e8c52004aa4af3eee6bebae2c5
Ben Sekyondwa <ben.sekyondwa@external.telekom.de>
2024-06-24 11:35:28 +0200

    launch screen and project settings

---
+++ OnlineGroceries/View/Login/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  OnlineGroceries
+//
+//  Created by Ben Sekyondwa on 24/06/2024.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
